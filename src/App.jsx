9874d6743adef0a8c416df87c3b53349898af184
Naveen-154 <sonspn23@gmail.com>
2024-05-24 09:20:05 +0530

    added css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './App.css'
+import GetAllMoviesComponent from './Components/GetAllMovieComponent/GetAllMovieComponent'
+import AddNewMovieComponent from './Components/AddNewMovieComponent/AddNewMovieComponent'
+import MovieSuggestionComponent from './Components/MovieSuggestionComponent/MovieSuggestionComponent'
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+            <div className="container">
+              <h1>Movie Suggestion App</h1>
+              
+            <nav className="nav-menu">
+                <Link to="/" >Home</Link>
+                <Link to="/admin/add" >Add Movies</Link>
+                <Link to="/suggest" >Get Suggestions</Link>
+            </nav>
+           <Routes>
+                 <Route exact path='/' element={<GetAllMoviesComponent/>}></Route>
+                 <Route path='/admin/add' element={<AddNewMovieComponent/>}></Route>
+                 <Route path='/suggest' element={<MovieSuggestionComponent/>}></Route>
+          </Routes>
+          </div>
+       </Router>
   )
 }
 
